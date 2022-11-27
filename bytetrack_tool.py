3c1933b7231279bb05069f286cf8595380e35def
Wang Yizhi <ghostqriver@hotmail.com>
2022-11-27 11:40:41 +0100

    Create bytetrack_tool.py

---
+++ bytetrack_tool.py
@@ -0,0 +1,14 @@
+import re
+import numpy as np
+
+def fps_from_log(log_path='log.txt'):
+  '''
+  Extract the avg fps from the bytetrack's log file
+  Please modify it or use your own FPS calculation funtion when the output file format is not suitable
+  '''
+  log_path='log.txt'
+  with open(log_path, 'r') as file:
+    text = file.read().replace('\n', '')
+  log_fps_s = re.findall(r'\(\d+\.\d+\sfps\)', text)
+  log_fps_s = list(map(lambda x : float(re.findall('\d+.\d+',x)[0]),log_fps_s))
+  return sum(log_fps_s[1:])/len(log_fps_s[1:])
